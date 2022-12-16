1e16918367ab70008af1a6cb32cb98bf5323c138
Xeraph <61700204+adrianrl99@users.noreply.github.com>
2022-12-16 09:45:41 -0500

    Initial Commit

---
+++ VSCode Profile Manager/VSCode_Profile_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VSCode_Profile_ManagerApp.swift
+//  VSCode Profile Manager
+//
+//  Created by Macbook on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct VSCode_Profile_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
